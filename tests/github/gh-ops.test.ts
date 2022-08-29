d9b1f4e8c391d4ae6509dad2ffbd20fb0ffff1a3
yeikel <email@yeikel.com>
2022-08-28 21:21:05 -0400

    feat: add option to set custom distributionsBaseUrl

--- tests/github/gh-ops.test.ts
+++ tests/github/gh-ops.test.ts
@@ -35,7 +35,8 @@ const defaultMockInputs: Inputs = {
   setDistributionChecksum: true,
   paths: [],
   pathsIgnore: [],
-  releaseChannel: ''
+  releaseChannel: '',
+  distributionsBaseUrl: ''
 };
 
 const defaultMockGitHubApi: IGitHubApi = {
